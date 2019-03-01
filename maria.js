a1bc756a9149cb0785ba09fb528c3b61d009cc98
mcervero77 <48137746+mcervero77@users.noreply.github.com>
2019-03-01 21:25:26 +0100

    Add files via upload

---
+++ maria.js
@@ -0,0 +1,9 @@
+function pie(nombre,emilio,fum)
+{
+	document.write('<br><table bgcolor=BLUE border=0 height=20 width=100% cellpadding=0 cellspacing=0><tr>');
+	document.write('<td align="center" valign="top"><p class=pie1>');
+	document.write('©'+' <a class=pie1 href="mailto:'+emilio+'">'+nombre+'</a>. ');
+	document.write('Powered by <a class=pie1 href="mailto:javier2@nevot-cercos.tk">J. Nevot</a>, '+fum+'. ');
+	document.write('Gracias por tu visita a mi página Web');
+	document.write('</p></td></tr></table>');
+}
